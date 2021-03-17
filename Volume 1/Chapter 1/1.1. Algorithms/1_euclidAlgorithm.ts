e2bbd5fa4cdf60556918db700c3e80b62208e3f1
Gabriel Nardes <gabrielnardeslobao@gmail.com>
2021-03-17 13:27:39 -0300

    Change export position

--- Volume 1/Chapter 1/1.1. Algorithms/1_euclidAlgorithm.ts
+++ Volume 1/Chapter 1/1.1. Algorithms/1_euclidAlgorithm.ts
@@ -1,5 +1,3 @@
-export {}; // to prevent "Duplicate function implementation" error
-
 /*
 Page 2
 
@@ -8,11 +6,13 @@ their greatest common divisor, that is, the largest positive integer that evenly
 divides both m and n.
 
 E1. [Find remainder.] Divide m by n and let r be the remainder. (We will have
-0 ≤ r < n.)
+    0 ≤ r < n.)
 E2. [Is it zero?] If r = 0, the algorithm terminates; n is the answer.
 E3. [Reduce.] Set m ← n, n ← r, and go back to step E1
 
 */
+
+export {}; // to prevent "Duplicate function implementation" error
 
 function printAnswer(
     functionName: string,
